36a87318b568fa7c2757ef76539318c4d8b2a8d5
Neonlab_kr <ksknk67@gmail.com>
2022-12-04 21:07:20 +0900

    DTO에 Argument와 ResultSet 변수 추가

--- GarageManagementProgram/src/Server/InfoDTO.java
+++ GarageManagementProgram/src/Server/InfoDTO.java
@@ -1,5 +1,6 @@
 package Server;
 import java.io.*;
+import java.sql.ResultSet;
 
 enum Info {
 	EXIT,TEST
@@ -7,11 +8,25 @@ enum Info {
 
 public class InfoDTO implements Serializable{
 	private Info command;
+	private ResultSet rs;
+	private String[] Argument;
 	
 	public Info getCommand() {
 		return command;
 	}
 	public void setCommand(Info command) {
 		this.command = command;
+	}
+	public ResultSet getRs() {
+		return rs;
+	}
+	public void setRs(ResultSet rs) {
+		this.rs = rs;
+	}
+	public String[] getArgument() {
+		return Argument;
+	}
+	public void setArgument(String[] argument) {
+		Argument = argument;
 	}
 }
